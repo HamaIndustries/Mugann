de88ba6e5067935619e1fc40ab60849354e692e8
hama <hama@hama.industries>
2026-03-25 18:30:50 -0700

    initial

---
+++ src/client/java/symbolics/division/MugannClient.java
@@ -0,0 +1,10 @@
+package symbolics.division;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MugannClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
